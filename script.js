e8ade0a867bcd09afa24c433ed4e2dcdbbb6421f
aishaimtiyaz <144771688+aishaimtiyaz@users.noreply.github.com>
2023-11-27 22:16:53 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,37 @@
-//complete this code
-class Rectangle {}
+class Rectangle {
+  constructor(width, height) {
+    this._width = width;
+    this._height = height;
+  }
 
-class Square extends Animal {}
+  get width() {
+    return this._width;
+  }
+
+  get height() {
+    return this._height;
+  }
+
+  getArea() {
+    return this._width * this._height;
+  }
+}
+
+class Square extends Rectangle {
+  constructor(side) {
+    // Call the constructor of the parent class (Rectangle) with the same argument for width and height
+    super(side, side);
+  }
+
+  // Override the getArea method if needed
+
+  getPerimeter() {
+    return 4 * this.width; // Since all sides of a square are equal, perimeter = 4 * side
+  }
+}
+
+// Example usage:
+
 
 // Do not change the code below this line
 window.Rectangle = Rectangle;
